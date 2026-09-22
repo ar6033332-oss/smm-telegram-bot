5a2ab6c36ba3b93d9c6e8d9d04bcd7c702299c43
ar6033332-oss <ar6033332@gmail.com>
2026-09-22 22:35:56 +0530

    Update sos.py

--- sos.py
+++ sos.py
@@ -12,8 +12,8 @@ from telebot import types
 BOT_TOKEN = "8203717604:AAEXt0oAR7FDbSoQ4pBZTZxXEQwJp6WyMOc"
 RENDER_URL = "https://smm-telegram-bot-w9s6.onrender.com"
 
-SMM_API_URL = "https://smmwiz.com/api/v2"
-SMM_API_KEY = "d0ee8a3432a6770f9a6003181d308d72"
+SMM_API_URL = "https://xmediasmm.in/api/v2"
+SMM_API_KEY = "08a1a294cbd54b19bdb1e5cf3c2682dc"
 
 ADMIN_ID = 6658716591
 UPI_ID = "arshad79@ptyes"
